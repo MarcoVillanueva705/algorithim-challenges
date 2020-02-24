bfbf0f68109a963f984a441a5c89aff322d13bc3
marco villanueva <villanuevamarco98@gmail.com>
2020-02-24 13:47:24 -0700

    First commit

---
+++ functions.js
@@ -0,0 +1,24 @@
+// const arr = [1, 2, 3, 4, 5, 6]
+
+// let a = arr[0]
+// let b = arr[1]
+
+// console.log(a)
+// console.log(b)
+
+
+NOTE //Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+
+// function frames(minutes, fps) {
+//     let result = minutes * (fps *60)
+//     return result;
+//     frames(10, 1)
+//     }
+
+NOTE //Create a function to concatenate two integer arrays.  This uses SPREAD syntax
+
+// function concat(arr1, arr2) {
+//     let arr3 = [...arr1, ...arr2]
+//         return arr3;
+//     }
+//     concat([7, 8], [10, 9, 1, 1, 2])
