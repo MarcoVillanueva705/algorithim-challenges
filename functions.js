c699830c6a17b501980f10b204a68ec19ab3cc4f
marco villanueva <villanuevamarco98@gmail.com>
2020-02-28 11:31:15 -0700

    friday first commit

--- functions.js
+++ functions.js
@@ -97,3 +97,13 @@ NOTE //Create a function that accepts an array and returns the last item in the 
 // 	}
 // }
 // getLastItem(["cat", "dog", "duck"])
+
+NOTE //In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. 
+//The farmer breeds three species: The farmer has counted his animals and he gives you a subtotal for each species. 
+//You have to implement a function that returns the total number of legs of all the animals.
+
+// function animals(chickens, cows, pigs) {
+// 	let totalLegs = (chickens * 2) + (cows * 4) + (pigs * 4);
+// 	return totalLegs;
+// }
+// animals(2, 3, 5)
