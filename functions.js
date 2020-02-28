dad95cb0907a5fb18fa0a0330f4c03b737058066
marco villanueva <villanuevamarco98@gmail.com>
2020-02-28 12:01:33 -0700

    third friday commit

--- functions.js
+++ functions.js
@@ -118,3 +118,9 @@ NOTE //Create a function that takes a number as an argument and returns "even" f
 // }
 // isEvenOrOdd(35)
 
+NOTE //There is an easy way to assign to array values to the nth index by using the Rest element.
+// Change the string so that it will evaluate to head = 1 and tail = [2, 3, 4] 
+// Use the Rest element
+
+// const str = `[head, ...tail] = [1, 2, 3, 4]`
+// console.log(str);
