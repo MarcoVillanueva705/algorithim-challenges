15e8a230016c72a40aa11050e353b3dd13620352
marco villanueva <villanuevamarco98@gmail.com>
2020-03-18 17:13:31 -0600

    new march commit file

---
+++ march.js
@@ -0,0 +1,13 @@
+//NOTE Create a function that returns true if two arrays contain identical values, and false otherwise.
+
+
+// function checkEquals(arr1, arr2) {
+// 	if (arr1.toString() === arr2.toString()) { //toString method returns a string representing the specified
+                                                //array and its elements
+//   	return true 
+//   } else {
+//   	return false
+//   }
+// }
+// checkEquals([4, 5, 6], [4, 5, 6])
+
