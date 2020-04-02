646f4609152cc2302218e50a94fd5990ca83270f
marco villanueva <villanuevamarco98@gmail.com>
2020-04-01 20:59:53 -0600

    added notes

--- april.js
+++ april.js
@@ -1,6 +1,7 @@
 //NOTE Create a function that returns the total number of parameters passed in.
 
-// function numberArgs(...things) {//The rest of the parameters can be included in the function definition by using three dots ... followed by the name of the array that will contain them. 
+// function numberArgs(...things) {//The rest of the parameters can be included in the function definition by using three dots ... followed 
+                                    //by the name of the array that will contain them. 
                                     //The dots literally mean “gather the remaining parameters into an array”.
 
 // 	return things.length
