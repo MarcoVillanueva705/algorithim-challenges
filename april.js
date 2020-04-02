9ba2506849ae515ebb42ed1a0ab09d7cf80b20a6
marco villanueva <villanuevamarco98@gmail.com>
2020-04-01 20:56:06 -0600

    moved func from april to march

---
+++ april.js
@@ -0,0 +1,6 @@
+
+
+//NOTE 
+
+                                        //The rest of the parameters can be included in the function definition by using three dots ... followed by the name of the array that will contain them. 
+                                        //The dots literally mean “gather the remaining parameters into an array”.
