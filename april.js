df542894c6e63356a44c3c10dad125b865b73755
marco villanueva <villanuevamarco98@gmail.com>
2020-04-03 10:46:16 -0600

    friday commit

--- april.js
+++ april.js
@@ -8,7 +8,8 @@
 // }
 // numberArgs(10, 20, 30, 40, 50)
                                         
-//NOTE
+//NOTE Write a function, .vreplace() that extends the String prototype by replacing all vowels 
+//in a string with a specified vowel.
 
 // String.prototype.vreplace = function(vowel) {
 // 	return this.replace(/[aeiou]/gi, vowel);
