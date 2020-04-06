9a61e084a3f442d40ed08b30f5902c3dbf4abf1f
marco villanueva <villanuevamarco98@gmail.com>
2020-04-06 12:25:46 -0600

    monday commit

--- april.js
+++ april.js
@@ -16,3 +16,14 @@
 // }
 // "cheese casserole".vreplace("o")
                                 
+//NOTE Create a function that takes two numbers and a mathematical operator + - / * and will perform
+// a calculation with the given numbers.
+
+// function calculator(num1, operator, num2) {
+// 	if(operator == '+') return num1+num2;
+// 	if(operator == '-') return num1-num2;
+// 	if(operator == '*') return num1*num2;
+// 	if(operator == '/' && num2 != 0) return num1/num2;
+// 	else return "Can't divide by 0!";
+// }
+// calculator(2, "/", 0) //If the input tries to divide by 0, return: "Can't divide by 0!"
