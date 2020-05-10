c5e9942953149e98f6f98e432f8053aa341254d9
marco villanueva <villanuevamarco98@gmail.com>
2020-05-10 17:33:00 -0600

    ans and notes added

--- may.js
+++ may.js
@@ -1,10 +1,24 @@
-function afterNYears(names, n) {
-	
-}
-afterNYears({
-    "Joel" : 32,
-    "Fred" : 44,
-    "Reginald" : 65,
-    "Susan" : 33,
-    "Julian" : 13
-  }, 1)
+//NOTE Given an object of people and their ages, return how old the people would be after n years have passed. 
+//Use the absolute value of n
+// function afterNYears(names, n) {
+// 	for (let k in names) { //The for...in statement iterates over all enumerable properties of an object that are keyed by strings
+// names[k] += Math.abs(n); //for every value of the names object keys i.e. "Joel" etc., add n to it
+                            //The Math.abs() function returns the absolute value of a number; it will never return a negative value
+// 	}
+// return names
+// }
+// afterNYears({
+//   "Joel" : 32,
+//   "Fred" : 44,
+//   "Reginald" : 65,
+//   "Susan" : 33,
+//   "Julian" : 13
+// }, 1)
+
+//ans {
+//   "Joel" : 33,
+//   "Fred" : 45,
+//   "Reginald" : 66,
+//   "Susan" : 34,
+//   "Julian" : 14
+// }
