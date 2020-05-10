8415349a5fdf8b831dce273e80ab3107903da387
marco villanueva <villanuevamarco98@gmail.com>
2020-05-10 17:00:01 -0600

    arguments passed in

--- may.js
+++ may.js
@@ -1,3 +1,16 @@
 function afterNYears(names, n) {
 	
 }
+afterNYears({
+    "Joel" : 32,
+    "Fred" : 44,
+    "Reginald" : 65,
+    "Susan" : 33,
+    "Julian" : 13
+  }, 1) ➞ {
+    "Joel" : 33,
+    "Fred" : 45,
+    "Reginald" : 66,
+    "Susan" : 34,
+    "Julian" : 14
+  }
