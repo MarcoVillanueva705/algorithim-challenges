25fe511fbada05005607b6a96f51749ac619ea6e
marco villanueva <villanuevamarco98@gmail.com>
2020-05-15 17:41:25 -0600

    comment out specialReverse function

--- may.js
+++ may.js
@@ -25,19 +25,19 @@
 
 //NOTE Write a function that reverses all the words in a sentence that start with a particular letter.
 
-function specialReverse(str, c) {
-	str = str.split(' ')//this turns str to "first", "man", "to", "walk", "on", "the", "moon"
-	for(let i in str){ //for variable in string; The for...in loop below iterates over all of 
-						//the string's enumerable, non-Symbol properties and logs a string of 
-						//the property names and their values.
-	  if(str[i].charAt(0) == c){ //if when iterating over each character, the one at the 0 index (or
-								//the first letter of the string), is the same as c parameter or "m" 
-								//argument
-		str[i] = str[i].split('').reverse().join('')//set str[i] i.e. "man" to a split i.e. "m","a","n" then reverse to
-													//"n","a","m" finally join str[i] to become "nam"
-	  }
-	}
-	return str.join(' ')//finally, return str but joined so it looks like one string, not separate strings i.e it
-						//now looks like "first nam to walk on the noom"
-   }
-specialReverse("first man to walk on the moon", "m")
+// function specialReverse(str, c) {
+// 	str = str.split(' ')//this turns str to "first", "man", "to", "walk", "on", "the", "moon"
+// 	for(let i in str){ //for variable in string; The for...in loop below iterates over all of 
+// 						//the string's enumerable, non-Symbol properties and logs a string of 
+// 						//the property names and their values.
+// 	  if(str[i].charAt(0) == c){ //if when iterating over each character, the one at the 0 index (or
+// 								//the first letter of the string), is the same as c parameter or "m" 
+// 								//argument
+// 		str[i] = str[i].split('').reverse().join('')//set str[i] i.e. "man" to a split i.e. "m","a","n" then reverse to
+// 													//"n","a","m" finally join str[i] to become "nam"
+// 	  }
+// 	}
+// 	return str.join(' ')//finally, return str but joined so it looks like one string, not separate strings i.e it
+// 						//now looks like "first nam to walk on the noom"
+//    }
+// specialReverse("first man to walk on the moon", "m")
