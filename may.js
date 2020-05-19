61c801d2dbf8676c5f4904a1626b123fb15dcafa
marco villanueva <villanuevamarco98@gmail.com>
2020-05-19 17:38:21 -0600

    function working

--- may.js
+++ may.js
@@ -44,7 +44,7 @@
 
 NOTE //Create a function that keeps only strings with repeating identical characters (in other words, it has a set size of 1).
 function identicalFilter(arr) {
-	let result = arr.filter(char => char === arr[0]);
+	let result = arr.filter(str => new Set(str).size == 1);
 
 	    return result;
 }
